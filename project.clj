44f526ab93f46a7f35b07435c67aaca559472816
Reid 'arrdem' McKenzie <rmckenzie@twitter.com>
2016-09-07 14:05:00 -0700

    Re-name artifact, bump deps

--- project.clj
+++ project.clj
@@ -1,5 +1,7 @@
-(defproject com.brweber2/clj-dns "0.0.3-SNAPSHOT"
-  :description "Clojure wrapper library for [dnsjava](http://www.xbill.org/dnsjava). Project at [clj-dns on github](http://github.com/brweber2/clj-dns)"
-  :dependencies [[org.clojure/clojure "1.3.0"]
-                 [dnsjava/dnsjava "2.1.1"]]
+(defproject me.arrdem/clj-dns "0.0.3-SNAPSHOT"
+  :description
+  "Clojure wrapper library for [dnsjava](http://www.xbill.org/dnsjava).
+ Project at [clj-dns on github](http://github.com/brweber2/clj-dns)"
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [dnsjava/dnsjava "2.1.7"]]
   :dev-dependencies [[lein-marginalia "0.7.0"]])
